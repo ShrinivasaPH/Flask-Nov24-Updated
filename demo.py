83cd7e71318ed4b2e2c87ceae2b48e430fea8bc4
ShrinivasaPH <134434204+ShrinivasaPH@users.noreply.github.com>
2024-12-03 20:35:57 +0530

    Create demo.py

---
+++ demo.py
@@ -0,0 +1,14 @@
+import requests
+
+url = 'http://127.0.0.1:5000/predict'
+
+data = {
+    "ApplicantIncome": 100,
+    "Credit_History": 1.0,
+    "Gender": "Male",
+    "LoanAmount": 12000,
+    "Married": "No"
+}
+
+x = requests.post(url, json=data)
+print(x.text)
